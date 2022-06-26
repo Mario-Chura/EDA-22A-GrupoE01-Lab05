d3e4e4c71f52764d43116869ebb050125d3da253
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-06-26 18:34:01 -0500

    implementacion del ejercicio

---
+++ Ejercicio_03_ArbolAVL/implementacion.java
@@ -0,0 +1,37 @@
+public class implementacion {
+
+    public static void main(String[] args) {
+       AVLTree<Integer> avl = new AVLTree<>();
+       try{
+            System.out.println("*Inserta: "+50);
+            avl.insert(50);
+            System.out.println(avl);
+            System.out.println("*Inserta: "+30);
+            avl.insert(30);
+            System.out.println(avl);
+            System.out.println("*Inserta: "+70);
+            avl.insert(70);
+            System.out.println(avl);
+            System.out.println("*Inserta: "+65);
+            avl.insert(65);
+            System.out.println(avl);
+            System.out.println("*Inserta: "+68);
+            avl.insert(68);
+            System.out.println(avl);
+            System.out.println("*Inserta: "+45);
+            avl.insert(45);                    
+            System.out.println(avl);
+            System.out.println("*Inserta: "+64);
+            avl.insert(64);                    
+            System.out.println(avl);
+            System.out.println("*Inserta: "+63);
+            avl.insert(63);                    
+            System.out.println(avl);
+            System.out.println("Dato: "+avl.search(40));
+            
+        }catch(Exception x){
+            System.out.println(x.getMessage());
+        }
+    }
+    
+}
