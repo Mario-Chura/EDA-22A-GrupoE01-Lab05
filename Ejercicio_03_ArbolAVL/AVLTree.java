6126d983d5feb517d52886e64e9d563e0da713bc
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-06-26 17:56:47 -0500

    metodo insertRec agregado

--- Ejercicio_03_ArbolAVL/AVLTree.java
+++ Ejercicio_03_ArbolAVL/AVLTree.java
@@ -7,6 +7,45 @@ public class AVLTree <T extends Comparable<T>>{
     public boolean isEmpty(){
         return this.root == null;
     }
+    private Node<T> insertRec(T x, Node<T> current) throws ItemDuplicated{
+        Node<T> res = current;
+        if(current == null){
+            this.height = true;
+            res = new Node(x);
+        }
+        else{
+            int resC= current.getData().compareTo(x);
+            if(resC == 0){
+                throw new ItemDuplicated("El Dato "+ x +" ya fue insertado antes ...");
+            }
+            if(resC < 0){
+                res.setRightNode(insertRec(x, current.getRightNode()));
+                if(this.height){
+                    switch(res.getBalanceFactor()){
+                        case -1: res.setBalanceFactor(0); this.height=false; break;
+                        case 0: res.setBalanceFactor(1);  this.height=true; break;
+                        case 1: 
+                                res = balanceToLeft(res);
+                                this.height=false;
+                                break;
+                    }
+                }
+            }
+            else{ 
+                res.setLeftNode(insertRec(x, current.getLeftNode()));
+                if(this.height){
+                    switch(res.getBalanceFactor()){
+                        case 1: res.setBalanceFactor(0); this.height=false; break;
+                        case 0: res.setBalanceFactor(-1); this.height=true; break;
+                        case -1: 
+                                res = balanceToRight(res);
+                                this.height=false; break;
+                    }
+                }
+            }
+        }
+        return res;
+    }
     
 
 }
