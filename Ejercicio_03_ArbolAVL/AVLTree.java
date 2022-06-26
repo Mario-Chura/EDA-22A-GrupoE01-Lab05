a84b9dad222c47b1d8d90a9471e02a22f5fdc117
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-06-26 17:53:23 -0500

    agregado atributo height

--- Ejercicio_03_ArbolAVL/AVLTree.java
+++ Ejercicio_03_ArbolAVL/AVLTree.java
@@ -1,10 +1,12 @@
 public class AVLTree <T extends Comparable<T>>{
     protected Node<T> root;
-        
+    private boolean height;     
     public AVLTree(){
         this.root=null;                      
     }
     public boolean isEmpty(){
         return this.root == null;
     }
+    
+
 }
