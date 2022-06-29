946055cf3bc7ee657df8a62faf3be1c2f8133fcf
Icielo23 <viquise@unsa.edu.pe>
2022-06-29 00:12:48 -0500

    Metodo isBalance completado

--- Stack.java
+++ Stack.java
@@ -1,22 +1,22 @@
-public class Stack <T extends Comparable<T>>{
+public class Stack <T> {
     private Nodo <T> root;
-public T pop(){
-    if (root!=null){
-        Nodo<T>Nodoeliminado=root;
-        root=root.getnextNodo();
-        Nodoeliminado.setnextNodo(null);
-        return Nodoeliminado.getdato();
+    public T pop(){
+        if (root!=null){
+            Nodo<T>Nodoeliminado=root;
+            root=root.getnextNodo();
+            Nodoeliminado.setnextNodo(null);
+            return Nodoeliminado.getdato();
+        }
+        return null;
     }
-    return null;
-}
-public void push (T dato){
-    Nodo <T> nodo=new Nodo<T>(dato);
-    nodo.setnextNodo(root);
-    root = nodo;
-}
-public T peek(){
-    if (root!=null)
-    return root.getdato();
-    return null;
-}
+    public void push (T dato){
+        Nodo <T> nodo=new Nodo<T>(dato);
+        nodo.setnextNodo(root);
+        root = nodo;
+    }
+    public T peek(){
+        if (root!=null)
+            return root.getdato();
+        return null;
+    }
 }
