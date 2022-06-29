5977faf92f735e2392c2167c7055cee6d95afd35
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-06-29 09:41:53 -0500

    implementacion completa del ejercicio

--- Ejercicio_03_ArbolAVL/implementacion.java
+++ Ejercicio_03_ArbolAVL/implementacion.java
@@ -1,37 +1,43 @@
 public class implementacion {
 
     public static void main(String[] args) {
-       AVLTree<Integer> avl = new AVLTree<>();
-       try{
-            System.out.println("*Inserta: "+50);
-            avl.insert(50);
-            System.out.println(avl);
-            System.out.println("*Inserta: "+30);
-            avl.insert(30);
-            System.out.println(avl);
-            System.out.println("*Inserta: "+70);
-            avl.insert(70);
-            System.out.println(avl);
-            System.out.println("*Inserta: "+65);
-            avl.insert(65);
-            System.out.println(avl);
-            System.out.println("*Inserta: "+68);
-            avl.insert(68);
-            System.out.println(avl);
-            System.out.println("*Inserta: "+45);
-            avl.insert(45);                    
-            System.out.println(avl);
-            System.out.println("*Inserta: "+64);
-            avl.insert(64);                    
-            System.out.println(avl);
-            System.out.println("*Inserta: "+63);
-            avl.insert(63);                    
-            System.out.println(avl);
-            System.out.println("Dato: "+avl.search(40));
-            
-        }catch(Exception x){
-            System.out.println(x.getMessage());
-        }
+        AVLTree<Integer> avl = new AVLTree<>();
+        try{
+             System.out.println("*Inserta: "+50);
+             avl.insert(50);
+             System.out.println(avl);
+             System.out.println("*Inserta: "+30);
+             avl.insert(30);
+             System.out.println(avl);
+             System.out.println("*Inserta: "+70);
+             avl.insert(70);
+             System.out.println(avl);
+             System.out.println("*Inserta: "+65);
+             avl.insert(65);
+             System.out.println(avl);
+             System.out.println("*Inserta: "+68);
+             avl.insert(68);
+             System.out.println(avl);
+             System.out.println("*Inserta: "+45);
+             avl.insert(45);                    
+             System.out.println(avl);
+             System.out.println("*Inserta: "+64);
+             avl.insert(64);                    
+             System.out.println(avl);
+             System.out.println("*Inserta: "+63);
+             avl.insert(63);                    
+             System.out.println(avl);
+             System.out.println("BUSQUEDA -> Dato: "+avl.search(63));
+             System.out.println("Remove: "+65);
+             avl.remove(65);
+             System.out.println(avl);
+             System.out.println("Remove: "+68);
+             avl.remove(68);
+             System.out.println(avl);
+             
+         }catch(Exception x){
+             System.out.println(x.getMessage());
+         }
     }
     
 }
